a4e67a8514d9b610b5de20332005a4091ea4cdd4
Benjamin A. Laken <benlaken@gmail.com>
2016-05-18 21:08:12 +0100

    setup info added

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ setup(
     description='To explore various personal financial scenarios',
     packages=find_packages(exclude=['*test']),
     scripts=[],
+    author_email='benlaken@icloud.com'
     author='Benjamin Laken',
     url='https://github.com/benlaken/fispy',
     license='by-nc-sa',
-    install_requires=['bokeh', 'pandas']
+    install_requires=['bokeh', 'pandas'],
+    zip_safe=False,
 )
