fc499ee9830d75fb5bbbd9095264e42cff5896cb
Benjamin A. Laken <benlaken@gmail.com>
2016-06-09 11:33:57 +0100

    bokeh app now working with right class

--- fispy/fispy_app.py
+++ fispy/fispy_app.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import datetime as dt
 from dateutil.relativedelta import relativedelta
 from bokeh.models import Button, HBox, VBoxForm
 from bokeh.models.widgets import Slider, Select, TextInput, RadioGroup
@@ -7,38 +8,59 @@ from bokeh.charts.attributes import cat, color
 from bokeh.plotting import Figure, curdoc, vplot
 from bokeh.models import ColumnDataSource
 from bokeh.io import vform
-from fispy.fispy import C1
+from fispy.fispy import Asset, Portfolio
 
-d = dict(
-        pay=[1.5, 1.5],
-        cash=19,
-        debt=70.,
-        repay=0.5,
-        expenses=0.7,
-        stocks=12,
-        max_cash=30,
-        stock_growth=0.04,
-        property_deposit=100,
-        second_flat_cost=200,
-        second_flat_monthly_payment=0.8,
-        rent_first_flat=0.8,
-        max_flats=1,
-        pay_debt_faster=True,
-        prd=50,
-        )
+# Initilise with list of assets...
+d1 = {'kind': 'job',
+      'monthly_income': 1.5,
+      'monthly_expenses': 0.7,
+      'start_date': dt.date(2016, 6, 1)}
 
-test = C1(d=d)
-source = ColumnDataSource(test.gen_quads())
+d2 = {'kind': 'job',
+      'monthly_income': 1.5,
+      'start_date': dt.date(2016, 12, 1)}
+
+d3 = {'kind': 'real estate',
+      'debt': 70,
+      'value': 150,
+      'monthly_repayment': 0.5,
+      'start_date': dt.date(2016, 6, 1),
+      'pay_debt_asap': True}
+
+d4 = {'kind': 'stocks',
+      'value': 15,
+      'symbol': 'NYSE:BRK.B'}
+
+d5 = {'kind': 'cash',
+      'value': 15,
+      'max_cash': 30}
+
+projection = Portfolio(Asset(**d1),
+                       Asset(**d2),
+                       Asset(**d3),
+                       Asset(**d4),
+                       Asset(**d5))
+source = ColumnDataSource(projection.gen_quads())
+
 TOOLS = "crosshair, pan, reset, resize, wheel_zoom"
-
 plot = Figure(tools=TOOLS, x_axis_type='datetime', plot_height=600,
               plot_width=600, title="FI calculator")
 p = plot.quad(left='left', right='right', bottom='bottom', top='top',
               color='color', source=source)
 
+print("Passed 1 point")
+
 def update_graphic():
-    test = C1(d=d)
-    bdf_quad = test.gen_quads()
+    """Update the data of the Asset dictionaries and recreate the
+    projection.
+    """
+    projection = Portfolio(Asset(**d1),
+                           Asset(**d2),
+                           Asset(**d3),
+                           Asset(**d4),
+                           Asset(**d5))
+    bdf_quad = projection.gen_quads()
+    #print(bdf_quad.head())
     source.data['bottom'] = bdf_quad['bottom']
     source.data['top'] = bdf_quad['top']
     source.data['left'] = bdf_quad['left']
@@ -47,34 +69,37 @@ def update_graphic():
 
 
 # Set up widgets
-debt_input = Slider(title="D start", value=d['debt'], start=0.0, end=200.0,
-                    step=1.0)
-debt_repay = Slider(title="D repay", value=d['repay'], start=0.0, end=5,
-                    step=0.1)
-expense_input = Slider(title="Monthly expenses", value=d['expenses'],
+debt_input = Slider(title="D start", value=d3['debt'],
+                    start=0.0, end=200.0, step=1.0)
+debt_repay = Slider(title="D repay", value=d3['monthly_repayment'],
+                    start=0.0, end=5, step=0.1)
+expense_input = Slider(title="Monthly expenses", value=d1['monthly_expenses'],
                        start=0, end=5, step=0.1)
-s1_input = Slider(title="s1", value=1.5, start=0.0, end=10.0)
-s2_input = Slider(title="s2", value=1.5, start=0.0, end=10.0)
-cash_start = Slider(title="Initial cash", value=d['cash'], start=0.0, end=100)
-cash_max = Slider(title="max_cash", value=d['max_cash'], start=0.0, end=200)
-# pay_faster = RadioGroup(
-#        labels=["PD faster", "Base only"], active=1)
-select = Select(title="Repay options:", value="base", options=["base", "ASAP"])
+s1_input = Slider(title="s1", value=d1['monthly_income'],
+                  start=0.0, end=10.0)
+s2_input = Slider(title="s2", value=d2['monthly_income'],
+                  start=0.0, end=10.0)
+cash_start = Slider(title="Initial cash", value=d5['value'],
+                    start=0.0, end=100)
+cash_max = Slider(title="max_cash", value=d5['max_cash'],
+                  start=0.0, end=200)
+select = Select(title="Repay options:", value="base",
+                options=["base", "ASAP"])
 
 
 def update_data(attrname, old, new):
-    # Get the current slider values
-    d['debt'] = debt_input.value
-    d['repay'] = debt_repay.value
-    d['cash'] = cash_start.value
-    d['max_cash'] = cash_max.value
-    d['pay'] = [s1_input.value, s2_input.value]
-    d['expenses'] = expense_input.value
+    """Set the current silder values as dictionary values"""
+    d3['debt'] = debt_input.value
+    d3['monthly_repayment'] = debt_repay.value
+    d5['value'] = cash_start.value
+    d5['max_cash'] = cash_max.value
+    d1['monthly_income'] = s1_input.value
+    d2['monthly_income'] = s2_input.value
+    d1['monthly_expenses'] = expense_input.value
     if select.value == 'base':
-        print("In logic ", select.value)
-        d['pay_debt_faster'] = False
+        d3['pay_debt_asap'] = False
     else:
-        d['pay_debt_faster'] = True
+        d3['pay_debt_asap'] = True
     update_graphic()
 
 
@@ -88,3 +113,4 @@ for w in interactive_list:
 # Set up layouts and add to document
 inputs = VBoxForm(children=interactive_list)
 curdoc().add_root(HBox(children=[inputs, plot], width=800))
+print("End of instruction list")
