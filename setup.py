ccfc6592063503ccfe359cc730e5de6cb8384b97
Benjamin A. Laken <benlaken@gmail.com>
2016-06-10 10:28:47 +0100

    tweaks for PyPI upload

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ setup(
     author_email='benlaken@icloud.com',
     author='Benjamin Laken',
     url='https://github.com/benlaken/fispy',
+    download_url="https://github.com/benlaken/fispy/tarball/0.1",
     license='by-nc-sa',
     install_requires=['bokeh', 'pandas'],
+    keywords=['finance', 'personal', 'independence'],
     zip_safe=False,
 )
