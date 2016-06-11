0eae15f2599e4586aca0484dbbbdab815630a982
Benjamin A. Laken <benlaken@gmail.com>
2016-06-11 18:44:13 +0100

    removed function from tests for old class

--- fispy/tests/test_fispy.py
+++ fispy/tests/test_fispy.py
@@ -2,30 +2,9 @@ import unittest
 import datetime as dt
 from fispy.fispy import C1, Asset, Portfolio
 
-d = dict(
-        pay=[1.5, 1.5],
-        cash=19,
-        debt=70.,
-        repay=0.5,
-        expenses=0.7,
-        stocks=12,
-        max_cash=30,
-        stock_growth=0.04,
-        property_deposit=100,
-        second_flat_cost=200,
-        second_flat_monthly_payment=0.8,
-        rent_first_flat=0.8,
-        max_flats=1,
-        pay_debt_faster=True,
-        prd=50,
-        )
-
 
 class TestFispyMethods(unittest.TestCase):
-
-    def test_C1objectCreated(self):
-        projection = C1(d=d)
-        self.assertIsNotNone(projection)
+    """Main class for fispy testing methods"""
 
     def test_PortfolioMonthlyIncome(self):
         d1 = {'kind': 'job',
